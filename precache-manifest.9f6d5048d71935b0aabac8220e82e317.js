a2ff76bdf6a803146340f34eb1a51fe25585b6cf
mohamed ali <52109014+momoboullo@users.noreply.github.com>
2020-02-26 18:13:23 +0530

    Add files via upload

---
+++ precache-manifest.9f6d5048d71935b0aabac8220e82e317.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e22e17f604a7b49a828524c300ac77d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9e6ecd05f411f10eb3c",
+    "url": "/static/css/main.c86d5f43.chunk.css"
+  },
+  {
+    "revision": "168f90691d3daa267123",
+    "url": "/static/js/2.bc89ab61.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.bc89ab61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9e6ecd05f411f10eb3c",
+    "url": "/static/js/main.45099bfd.chunk.js"
+  },
+  {
+    "revision": "8d0a13c6d7ecddcb2ca8",
+    "url": "/static/js/runtime-main.014ddd74.js"
+  },
+  {
+    "revision": "70ca5e0489050900d1ae103627a28db2",
+    "url": "/static/media/idroservice.70ca5e04.jpg"
+  }
+]);
